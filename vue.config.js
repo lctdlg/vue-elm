1d2c67ca714c0ff824c650ce7ceb2c1df1a1d1c0
lctdlg <2669022410@qq.com>
2025-04-06 19:52:33 +0800

    featf: 添加环境变量

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,31 @@
-const { defineConfig } = require("@vue/cli-service");
-const appData = require("./data.json");
-const seller = appData.seller;
-const goods = appData.goods;
-const ratings = appData.ratings;
-const foods = appData.foods;
+const { defineConfig } = require('@vue/cli-service')
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+const foods = appData.foods
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/vue-elm/' // 替换为你的仓库名称
+      : '/',
   transpileDependencies: true,
   devServer: {
     // 代理请求数据
     setupMiddlewares: (middlewares, devServer) => {
-      devServer.app.get("/api/seller", (_req, res) => {
-        res.json({ errno: 0, data: seller });
-      });
-      devServer.app.get("/api/goods", (_req, res) => {
-        res.json({ errno: 0, data: goods });
-      });
-      devServer.app.get("/api/ratings", (_req, res) => {
-        res.json({ errno: 0, data: ratings });
-      });
-      devServer.app.get("/api/foods", (_req, res) => {
-        res.json({ errno: 0, data: foods });
-      });
-      return middlewares;
-    },
-  },
-});
+      devServer.app.get('/api/seller', (_req, res) => {
+        res.json({ errno: 0, data: seller })
+      })
+      devServer.app.get('/api/goods', (_req, res) => {
+        res.json({ errno: 0, data: goods })
+      })
+      devServer.app.get('/api/ratings', (_req, res) => {
+        res.json({ errno: 0, data: ratings })
+      })
+      devServer.app.get('/api/foods', (_req, res) => {
+        res.json({ errno: 0, data: foods })
+      })
+      return middlewares
+    }
+  }
+})
